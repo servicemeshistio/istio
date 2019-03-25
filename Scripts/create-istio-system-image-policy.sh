83ba8c6059e1f6e981c3485d970d2057f1c1ce37
VIKRAM KHATRI <vikram.khatri@us.ibm.com>
2019-03-24 21:18:02 -0400

    First commit

--- Scripts/create-istio-system-image-policy.sh
+++ Scripts/create-istio-system-image-policy.sh
@@ -1,13 +1,13 @@
 echo Create image policy to download docker images from docker.io/*
-kubectl create -f - << EOF
+kubectl -n istio-system create -f - << EOF
 apiVersion: securityenforcement.admission.cloud.ibm.com/v1beta1
 kind: ClusterImagePolicy
 metadata:
- name: docker-image-policy
- spec:
+ name: istio-system-image-policy
+spec:
  repositories:
  - name: docker.io/*
- policy:
- va:
-  enabled: false
+   policy:
+    va:
+     enabled: false
 EOF
